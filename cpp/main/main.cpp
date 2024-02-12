bf5980ea83213cbaf701070d3803b9bc2d3a3c9f
Ruşen <are255gu@gmail.com>
2024-02-12 16:21:03 +0300

    cpp dosyası yazıldı

--- cpp/main/main.cpp
+++ cpp/main/main.cpp
@@ -1,5 +1,52 @@
 #include <iostream>
+using std::string;
+class Vehicle{
+private:
+    string manifacturer;
+    int year;
+    string color;
+public:
+    string getmanifacturer(){
+        return manifacturer;
+    }
 
+    void setManifacturer(string value){
+
+        this-> manifacturer=value;
+
+    }
+    int getYear(){
+        return this->year;
+    }
+
+    void setYear(int value){
+        this-> year=value;
+    }
+    
+    string getColor(){
+        return this-> color;
+    }
+
+    void setColor(string value){
+        this-> color=value;
+    }
+
+    Vehicle(){
+        std::cout<<"\nAn instance has been derived from Vehicle.";    
+    }
+    Vehicle(string manifacturer, int year, string color) : Vehicle(){
+        this->manifacturer = manifacturer;
+        this->year=year;
+        this->color=color;
+
+    }
+};
 int main(){
-    return -1;
+    Vehicle item1 = Vehicle();
+    item1.setManifacturer("volvo");//set yazılması
+    std::cout<<"Vehicle : " << item1.getmanifacturer() << std::endl;
+
+    Vehicle item2 =Vehicle("Scode",2019,"White");
+    std::cout<<"Vehicle " << item2.getmanifacturer()<< std::endl;
+    return 0;
 }
